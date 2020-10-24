7f2d61951d019c7c7ff7d999746b071d4012297f
abelfubu <abelfubu@gmail.com>
2020-10-24 20:55:44 +0200

    Added particles.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as particlesJS from 'particles.js';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bubble-sort';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    particlesJS.load('particles-js', 'assets/particles.json', function () {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
